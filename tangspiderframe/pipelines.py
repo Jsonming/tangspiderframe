3c4caf810776b2796c7bf651e578dc0230bc22b0
jsonming <ymmbjcz@126.com>
2019-12-10 11:00:58 +0800

    20191210b

--- tangspiderframe/pipelines.py
+++ tangspiderframe/pipelines.py
@@ -27,7 +27,7 @@ class SSDBPipeline(object):
 
     def close_spider(self, spider):
         dd = DingDing()
-        dd.send(spider.name, "")
+        dd.send(spider.name, "完成")
         self.ssdb_conn.close()
 
     def process_item(self, item, spider):
