775878862978c287379281f0dc1aeb569a18574f
XinYuCode <1582166721@qq.com>
2019-12-11 11:38:36 +0800

    20131211

--- tangspiderframe/spiders/text_english_chinadaily_link.py
+++ tangspiderframe/spiders/text_english_chinadaily_link.py
@@ -6,12 +6,43 @@ from tangspiderframe.items import TangspiderframeItem
 class TextEnglishChinadailyLinkSpider(scrapy.Spider):
     name = 'text_english_chinadaily_link'
     allowed_domains = ['www.chinadaily.com.cn']
-    start_urls = ['http://www.chinadaily.com.cn/world/asia_pacific/page_{}.html'.format(i) for i in range(1, 3000)]
+    start_urls = ['http://www.chinadaily.com.cn/']
 
-    def parse(self, response):
-        links = response.xpath('//a[@shape="rect"]/@href').extract()
-        for link in links:
-            url = "http:" + link
-            item = TangspiderframeItem()
-            item["url"] = url
-            yield item
+    # def parse(self, response):
+    #     links = response.xpath('//ul[@class="dropdown"]/li[position()>3]/a/@href').extract()
+    #     for link in links:
+    #         if "http" not in link:
+    #             link = "http:"+link
+    #             yield scrapy.Request(url=link, callback=self.parse_link, dont_filter=True)
+    #
+    # def parse_link(self, response):
+    #     links = response.xpath('//div[@class="topNav2_art"]/ul/li/a/@href').extract()
+    #     for link in links:
+    #         if "http" not in link:
+    #             link = "http:" + link
+    #             yield scrapy.Request(url=link, callback=self.parse_url, dont_filter=True)
+    #
+    # def parse_url(self, response):
+    #     next_links = response.xpath('//div[@id="div_currpage"]/a[@class="pagestyle"]/@href').extract()
+    #     for next_link in next_links:
+    #         next_link = "http:" + next_link
+    #         yield scrapy.Request(url=next_link, callback=self.parse_url, dont_filter=True)
+    #
+    #     links = response.xpath('//h4/a[@shape="rect"]/@href').extract()
+    #     for link in links:
+    #         url = "http:" + link
+    #         yield scrapy.Request(url=url, callback=self.parse_content, dont_filter=True)
+
+
+    # def parse_url(self, response):
+    #     next_links = response.xpath('//div[@id="div_currpage"]/a[@class="pagestyle"]/@href').extract()
+    #     for next_link in next_links:
+    #         next_link = "http:"+next_link
+    #         yield scrapy.Request(url=next_link,callback=self.parse,dont_filter=True)
+    #
+    #     links = response.xpath('//h4/a[@shape="rect"]/@href').extract()
+    #     for link in links:
+    #         url = "http:" + link
+    #         item = TangspiderframeItem()
+    #         item["url"] = url
+    #         yield item
