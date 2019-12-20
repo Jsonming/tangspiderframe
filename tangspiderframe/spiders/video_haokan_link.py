12d715f73b2efdbace7cdf68b7e3a0e68230d1f1
XinYuCode <1582166721@qq.com>
2019-12-20 15:04:36 +0800

    20131220a

--- tangspiderframe/spiders/video_haokan_link.py
+++ tangspiderframe/spiders/video_haokan_link.py
@@ -1,31 +1,49 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import time
 from tangspiderframe.items import TangspiderframeItem
 
 class VideoHaoKanLinkSpider(scrapy.Spider):
-    name = 'video_haokan_link'
+    name = 'video_haokan1_link'
 
+    # def start_requests(self):
+    #     for pn in range(2,5):
+    #         # url = "https://haokan.baidu.com/videoui/page/pc/search?pn={pn}&rn=10&_format=json&tab=video&query=%E5%8C%96%E5%A6%86".format(pn=pn)
+    #         url = "https://haokan.baidu.com/videoui/page/pc/search?pn={pn}&rn=10&_format=json&tab=video&query=%E5%8C%96%E5%A6%86"
+    #         yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+    #
+    # def parse(self, response):
+    #     resp = json.loads(response.text)
+    #     data = resp.get("data")
+    #     url_lists = data["response"]["list"]
+    #     for url_list in url_lists:
+    #         url = url_list["url"]
+    #         if url:
+    #             yield scrapy.Request(url=url, callback=self.parse_url, dont_filter=True)
+    #
+    # def parse_url(self, response):
+    #     links = response.xpath('//video/@src').extract()
+    #     for link in links:
+    #         item = TangspiderframeItem()
+    #         item['url'] = link
+    #         # print(item)
+    #         yield item
 
     def start_requests(self):
-        for pn in range(2,5):
-            url = "https://haokan.baidu.com/videoui/page/pc/search?pn={pn}&rn=10&_format=json&tab=video&query=%E5%8C%96%E5%A6%86".format(pn=pn)
+        for i in range(0,100):
+            timestamp = int(round(time.time() * 1000))
+            url = "https://haokan.baidu.com/videoui/api/videorec?tab=shishang&act=pcFeed&pd=pc&num=20&shuaxin_id={timestamp}".format(timestamp=timestamp)
             yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
 
     def parse(self, response):
         resp = json.loads(response.text)
         data = resp.get("data")
-        url_lists = data["response"]["list"]
+        url_lists = data["response"]["videos"]
         for url_list in url_lists:
-            url = url_list["url"]
-            if url:
-                yield scrapy.Request(url=url, callback=self.parse_url, dont_filter=True)
-
-    def parse_url(self, response):
-        links = response.xpath('//video/@src').extract()
-        for link in links:
+            url = url_list["play_url"]
             item = TangspiderframeItem()
-            item['url'] = link
+            item['url'] = url
             # print(item)
             yield item
 
