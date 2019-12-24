b11824b3f21cb9004f449d65a277aab92ebf0ce0
XinYuCode <1582166721@qq.com>
2019-12-24 14:25:56 +0800

    20131224c

--- tangspiderframe/spiders/text_canada_journaldemontreal_link.py
+++ tangspiderframe/spiders/text_canada_journaldemontreal_link.py
@@ -4,16 +4,12 @@ from tangspiderframe.items import TangspiderframeItem
 
 
 class TextCanadaJournaldemontrealLinkSpider(scrapy.Spider):
-    name = 'text_canada_journaldemontreal_enquetes_link'
+    name = 'text_canada_journaldemontreal_porte_link'
     # start_urls = ["https://www.journaldemontreal.com/"]
 
     #     urls=["""
     # https://www.journaldemontreal.com/actualite
-    # https://www.journaldemontreal.com/spectacles
-    # https://www.journaldemontreal.com/argent
-    # https://www.journaldemontreal.com/auto
     # https://www.journaldemontreal.com/monde
-    # https://www.journaldemontreal.com/jm
     # https://www.journaldemontreal.com/porte-monnaie
     # https://www.journaldemontreal.com/maison-extra
     # https://www.journaldemontreal.com/5-minutes
@@ -23,12 +19,12 @@ class TextCanadaJournaldemontrealLinkSpider(scrapy.Spider):
     # https://www.journaldemontreal.com/24heures"""]
 
     def start_requests(self):
-        for year in range(2013, 2019):
+        for year in range(2018, 2019):
             for month in range(1, 13):
                 month = str(month).zfill(2)
                 for day in range(1, 31):
                     day = str(day).zfill(2)
-                    url = "https://www.journaldemontreal.com/enquetes/archives/{year}/{month}/{day}".format(
+                    url = "https://www.journaldemontreal.com/porte-monnaie/archives/{year}/{month}/{day}".format(
                         year=year, month=month, day=day)
                     yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
 
