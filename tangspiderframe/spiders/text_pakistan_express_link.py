e5be6bb2a6fec12ee93071dec9952ad58548d967
XinYuCode <1582166721@qq.com>
2019-12-25 10:52:47 +0800

    20131225a

--- tangspiderframe/spiders/text_pakistan_express_link.py
+++ tangspiderframe/spiders/text_pakistan_express_link.py
@@ -4,7 +4,7 @@ from tangspiderframe.items import TangspiderframeItem
 
 
 class TextPakistanExpressLinkSpider(scrapy.Spider):
-    name = 'text_pakistan_express_science_link'
+    name = 'text_pakistan_express_business_link'
     handle_httpstatus_list = [404,429]
     # time.sleep()
 
@@ -20,7 +20,7 @@ class TextPakistanExpressLinkSpider(scrapy.Spider):
 
     def start_requests(self):
         for i in range(1,300):
-            url = "https://www.express.pk/science/archives/?page={i}".format(i=i)
+            url = "https://www.express.pk/business/archives/?page={i}".format(i=i)
             yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
 
     def parse(self, response):
