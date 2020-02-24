fdc66475439a2b1d1c9c6ba1c78278953198ebf4
jsonming <ymmbjcz@126.com>
2020-02-24 17:40:39 +0800

    20200224

--- tangspiderframe/spiders/text_china_dxy_content.py
+++ tangspiderframe/spiders/text_china_dxy_content.py
@@ -3,12 +3,23 @@ import scrapy
 import json
 import re
 from tangspiderframe.items import TangspiderframeItem
+from scrapy_redis.spiders import RedisSpider
 
 
-class TextChinaDxyContentSpider(scrapy.Spider):
+class TextChinaDxyContentSpider(RedisSpider):
     name = 'text_china_dxy_content'
     allowed_domains = ['ask.dxy.com']
     start_urls = ['https://ask.dxy.com/question/3123123']
+
+    redis_key = "text_china_dxy_link"
+    custom_settings = {
+        'REDIS_HOST': '123.56.11.156',
+        'REDIS_PORT': 8888,
+        'REDIS_PARAMS': {
+            'password': '',
+            'db': 0
+        },
+    }
 
     def parse(self, response):
         # 结构化内容
@@ -31,7 +42,7 @@ class TextChinaDxyContentSpider(scrapy.Spider):
                 else:
                     new_content = "patient:" + dialog_content
                 dialogs.append(new_content)
-            return  "\t".join(dialogs)
+            return "\t".join(dialogs)
 
         dialog = parse_dialog(questionDialog)
 
@@ -42,4 +53,3 @@ class TextChinaDxyContentSpider(scrapy.Spider):
         item['category'] = disease
         item['content'] = dialog
         yield item
-
